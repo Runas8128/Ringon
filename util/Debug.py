51f8f1eda224e9df69fcb0a29223db99e28738b8
Runas8128 <minseongexcalibur@naver.com>
2022-07-22 23:42:40 +0900

    make Count Chat amount command

--- util/Debug.py
+++ util/Debug.py
@@ -1,7 +1,12 @@
 from typing import List
+import asyncio
+from datetime import timedelta
+from time import time
 
 import discord
 from discord.ext import commands
+
+from util import utils
 
 class CogDebug(commands.Cog):    
     def __init__(self, bot: commands.Bot):
@@ -15,15 +20,20 @@ class CogDebug(commands.Cog):
     
     @commands.Cog.listener()
     async def on_ready(self):
-        self.guild = self.bot.get_guild(758478112979288094)
-        self.bugReportChannel: discord.TextChannel = self.bot.get_channel(884356850248724490)
-        self.ServerNotice: discord.TextChannel = self.bot.get_channel(765759817662857256)
+        self.guild: discord.Guild = self.bot.get_guild(758478112979288094)
         
         self.IgnoreRole: List[discord.Role] = [
             self.guild.get_role(924315254098387024), # Guest of Honor
             self.guild.get_role(861883220722319391), # 군머
             self.guild.get_role(805451727859613707)  # 고3
         ]
+
+        self.Category: List[discord.CategoryChannel] = [
+            self.bot.get_channel(891697283702345798), # LAB
+            self.bot.get_channel(982903025322577940)  # Rating
+        ]
+
+        self.NotNotifyRoom: discord.TextChannel = self.bot.get_channel(809342346466557952)
     
     @commands.command(name='인원점검')
     @commands.has_permissions(administrator=True)
@@ -101,6 +111,56 @@ class CogDebug(commands.Cog):
                 await notMsg.edit(content="", embed=embed)
         else:
             await ctx.send("사용법: `!인원점검 시작` / `!인원점검 끝 (메시지 아이디)`")
+    
+    @commands.command(name="채팅량검사")
+    @commands.has_permissions(administrator=True)
+    async def RG_CheckChatAmount(self, ctx: commands.Context):
+        tmp: discord.Message = await ctx.send(embed=discord.Embed(
+            title="각 멤버의 채팅량을 불러오는 중입니다.",
+            description=f"각 멤버에 대한 로그는 {self.NotNotifyRoom.mention}에서 봐주세요!"
+        ))
+        b = time()
+        await asyncio.gather([
+            self.getUserChatAmount(member) for member in self.guild.members
+        ])
+        e = time()
+        await tmp.edit(embed=discord.Embed(
+            title="전체 멤버의 채팅량을 불러왔습니다!",
+            description=f"각 멤버의 채팅량은 {self.NotNotifyRoom.mention}을 참고해주세요!\n걸린 시간: {round(e - b, 2):.2f}초"
+        ))
+
+    async def getUserChatAmount(self, member: discord.Member):
+        """Count chat amount in `Lab` and `Rating` category and send count stack info to not-notifing-admin room.
+
+        Parameters
+        ----------
+        * member: :class:`discord.Member`
+            - Member object to get chat amount
+
+        ."""
+        noticeMessage: discord.Message = await self.NotNotifyRoom.send(f"{member.mention}님의 역할을 분석하는 중...")
+
+        for role in self.IgnoreRole:
+            if role in member.roles:
+                await noticeMessage.edit(content=f"{member.mention}({role.name})님의 채팅량 검사를 건너뜁니다.")
+                return
+            
+        def predicate(message: discord.Message):
+            return message.author.id == member.id
+
+        count = 0
+        channel: discord.TextChannel
+        for category in self.Category:
+            for channel in category.channels:
+                if not isinstance(channel, discord.TextChannel):
+                    continue
+                
+                message: discord.Message
+                async for _ in channel.history(after=now-timedelta(days=14), limit=50).find(predicate):
+                    count += 1
+                    if count >= 50:
+                        await noticeMessage.edit(content=f"{member.mention}님의 채팅량: 50개 이상, 검사를 중단합니다.")
+        await noticeMessage.edit(content=f"{member.mention}님의 채팅량은 {count}건입니다.")
 
 def setup(bot):
     bot.add_cog(CogDebug(bot))
