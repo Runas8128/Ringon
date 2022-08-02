9018bea681157bec4ba3669518cc0044b8d2b997
Runas8128 <minseongexcalibur@naver.com>
2022-08-02 12:14:43 +0900

    split birthday database into `util` package

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -4,29 +4,8 @@ import discord
 from discord.ext import commands, tasks
 
 from util import utils
-from util.baseDB import DB
+from util.birthday import birthdayDB
 from util.myBot import MyBot
-
-class birthdayDB(DB):
-    def __init__(self):
-        super().__init__("DB/birthday.db")
-    
-    def getToday(self, now) -> List[int]:
-        """get IDs for members whom birthday is today
-
-        Parameters
-        ----------
-        * now: :class:`datetime.datetime`
-            - datetime object that refers to now
-
-        ."""
-        date = now.strftime("%m/%d")
-        return [
-            val["ID"] for val in
-            self._runSQL("SELECT ID FROM BIRTHDAY WHERE date=?", date)
-        ]
-
-db = birthdayDB()
 
 class Birthday(commands.Cog):
     def __init__(self, bot: MyBot):
@@ -44,7 +23,7 @@ class Birthday(commands.Cog):
         now = utils.now()
         if (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).seconds >= 1*60*60: return
         
-        members: List[discord.Member] = [self.guild.get_member(id) for id in db.getToday(now)]
+        members: List[discord.Member] = [self.guild.get_member(id) for id in birthdayDB.getToday(now)]
         if len(members) == 0: return
 
         for member in members:
