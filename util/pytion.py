cdbbaea1529c46448e1da4e38e81fc5ae2e621d6
Runas8128 <runas8128@naver.com>
2022-08-14 10:21:21 +0900

    add default to `Version`, shorten name, and add ID map

--- util/pytion.py
+++ util/pytion.py
@@ -1,25 +1,23 @@
 from typing import Optional, Callable
-from enum import Enum
-
 import json, httpx
 
 from .load_token import load_token
 
-def get_text_from_richtext(richTextObj: dict):
+def parse_richtext(richTextObj: dict):
     if 'title' in richTextObj:
         richTextObj = richTextObj['title']
     if 'rich_text' in richTextObj:
         richTextObj = richTextObj['rich_text']
     return richTextObj[0]['plain_text']
 
-class Version(Enum):
-    v1 = "2021-05-13"
-    v2 = "2021-08-16"
-    v3 = "2022-02-22"
-    v4 = "2022-06-28"
+class Version:
+    v1 =            "2021-05-13"
+    v2 =            "2021-08-16"
+    v3 = default =  "2022-02-22"
+    v4 =            "2022-06-28"
 
 class Notion:
-    def __init__(self, *, version: Version = Version.v3, is_testing: bool=False):
+    def __init__(self, *, version: Version = Version.default, is_testing: bool=False):
         self.client = httpx.Client(
             base_url="https://api.notion.com/v1",
             headers={
@@ -81,11 +79,11 @@ class Notion:
     
     def get_block_text(self, blockID: str):
         content = self.request(method="GET", url=f"/blocks/{blockID}").json()
-        return get_text_from_richtext(content['paragraph'])
+        return parse_richtext(content['paragraph'])
     
     def get_block_text_list(self, rootBlockID: str):
         content = self.request(method="GET", url=f"/blocks/{rootBlockID}/children").json()
-        return [get_text_from_richtext(item['paragraph']) for item in content['results']]
+        return [parse_richtext(item['paragraph']) for item in content['results']]
 
     def update_block_text(self, blockID: str, newText: str):
         resp = self.request(
@@ -119,3 +117,20 @@ class Notion:
         )
 
         return resp.is_success
+
+class ID:
+    class database:
+        class deck:
+            data    = "8ef3fed5a101492882ba96dab96c096b"
+            contrib = "7a5aec5497b64aa5a9e0942fbeb81c97"
+        
+        class detect:
+            full    = "481b6de47c6a41debf83fe1b93700622"
+            part    = "371aa34963db4efa9b3d4b398aa3c46b"
+            prob    = "5e22777f724b4f27a336cdce350bc1a2"
+        
+        birthday    = "a66f05422b91471da108737db205c7c7"
+    
+    class block:
+        pack        = "b65b0b9652d04da58f960045aa01568c"
+        word        = "233288a3891f4c008504470c8fbefc88"
