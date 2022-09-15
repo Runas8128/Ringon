0c928ccf1b9c2824c76caeb50f4842c6655a52d3
Runas8128 <runas8128@naver.com>
2022-09-15 21:04:03 +0900

    getting log is free for everyone

--- cogs/Debug.py
+++ cogs/Debug.py
@@ -12,14 +12,7 @@ class CogDebug(commands.Cog):
         name="log",
         description="send log file"
     )
-    @app_commands.default_permissions(
-        administrator=True
-    )
     async def log(self, interaction: discord.Interaction):
-        if not interaction.user.guild_permissions.administrator:
-            return await interaction.response.send_message(
-                "관리자 전용 명령어입니다."
-            )
         await interaction.response.send_message(
             file=discord.File('.log'),
             ephemeral=True
